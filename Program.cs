4cf2de9fe93aa490b6d0072fa15d60e75d3e63bc
AndreyKerges <andrey.nascimento4@etec.sp.gov.br>
2023-04-02 12:58:41 -0300

    exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//declara as variáveis do tipo int
+int valorc, vpago, troco;
+
+//exibe a mensagem "Digite o valor da compra:" no console
+Console.WriteLine("Digite o valor da compra: ");
+//Lê o valor digitado pelo usuário e converte para o 
+//tipo int, atribuindo-o para a variável valorc
+valorc = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o valor pago: ");
+vpago = Convert.ToInt32(Console.ReadLine());
+
+//Cálculo do valor do troco, subtraindo o valor da compra 
+//do valor pago e armazenando o resultado na variável troco.
+troco = (vpago - valorc);
+Console.WriteLine($"Seu troco é: {troco}");
